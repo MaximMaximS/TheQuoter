c107a69121893fbc311ca50ff1e8f69d09052420
MaximMaximS <sklenicka.maxim@gmail.com>
2022-06-12 11:41:19 +0200

    Add cors

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import "dotenv/config";
 import express from "express";
 import ratelimit from "express-rate-limit";
@@ -22,6 +23,7 @@ async function main() {
 
   // Security headers
   app.use(helmet());
+  app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
 
