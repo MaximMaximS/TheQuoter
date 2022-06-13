f36a08df7a79a4cdd60133be9ea1498dc4004151
MaximMaximS <sklenicka.maxim@gmail.com>
2022-06-13 16:27:16 +0200

    Add jest

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+// eslint-disable-next-line unicorn/prefer-module
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+};
