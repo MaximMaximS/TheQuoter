4bf6e9f14f482effb98ea8c4930698936f01fa90
MaximMaximS <sklenicka.maxim@gmail.com>
2022-07-08 18:00:26 +0200

    Remove useless file

--- src/custom.d.ts
+++
@@ -1,8 +0,0 @@
-import { IUser } from "./modules/models/user";
-
-// Ensures that request allows user property to be set
-declare namespace Express {
-  export interface Request {
-    user: IUser | null;
-  }
-}
